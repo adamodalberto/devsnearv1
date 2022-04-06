6b64b666cf2f3b2797a23462a0da149411dd8b17
Snear Team <adamo.dalberto@gmail.com>
2022-04-06 16:49:12 -0300

    Ambiente Desenvolvimento Snear Team

---
+++ src/server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Testando')
+})
+
+app.listen(port, () => {
+  console.log(`Testando o container DEV ${port}`)
+})
+
